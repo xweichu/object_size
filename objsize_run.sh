1c4d55a18e3449d7810929f91d0fc2ac02a97c03
Aaron Chu <xweichu@hotmail.com>
2018-09-25 21:46:29 -0700

    use rate_iops to do the test

--- objsize_run.sh
+++ objsize_run.sh
@@ -11,30 +11,31 @@ ssh -p 22 $client << 'EOF'
     rm /mnt/cephfs/FIO/32MB/Test.0.0
     rm /mnt/cephfs/FIO/64MB/Test.0.0
 
-    fio /users/xweichu/FIO/global.fio --rw=read --bs=256k --directory=/mnt/cephfs/FIO/0.25MB/ --output-format=json --output=/users/xweichu/object_size/data/R0.25MB.json --iodepth=1
-    fio /users/xweichu/FIO/global.fio --rw=read --bs=512k --directory=/mnt/cephfs/FIO/0.5MB/ --output-format=json --output=/users/xweichu/object_size/data/R0.5MB.json --iodepth=1
-    fio /users/xweichu/FIO/global.fio --rw=read --bs=1024k --directory=/mnt/cephfs/FIO/1MB/ --output-format=json --output=/users/xweichu/object_size/data/R1MB.json --iodepth=1
-    fio /users/xweichu/FIO/global.fio --rw=read --bs=2048k --directory=/mnt/cephfs/FIO/2MB/ --output-format=json --output=/users/xweichu/object_size/data/R2MB.json --iodepth=1
-    fio /users/xweichu/FIO/global.fio --rw=read --bs=4096k --directory=/mnt/cephfs/FIO/4MB/ --output-format=json --output=/users/xweichu/object_size/data/R4MB.json --iodepth=1
-    fio /users/xweichu/FIO/global.fio --rw=read --bs=8192k --directory=/mnt/cephfs/FIO/8MB/ --output-format=json --output=/users/xweichu/object_size/data/R8MB.json --iodepth=1
-    fio /users/xweichu/FIO/global.fio --rw=read --bs=16384k --directory=/mnt/cephfs/FIO/16MB/ --output-format=json --output=/users/xweichu/object_size/data/R16MB.json --iodepth=1
-    fio /users/xweichu/FIO/global.fio --rw=read --bs=32768k --directory=/mnt/cephfs/FIO/32MB/ --output-format=json --output=/users/xweichu/object_size/data/R32MB.json --iodepth=1
-    fio /users/xweichu/FIO/global.fio --rw=read --bs=65536k --directory=/mnt/cephfs/FIO/64MB/ --output-format=json --output=/users/xweichu/object_size/data/R64MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=read --bs=256k --directory=/mnt/cephfs/FIO/0.25MB/ --output-format=json --output=/users/xweichu/object_size/data/R0.25MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=read --bs=512k --directory=/mnt/cephfs/FIO/0.5MB/ --output-format=json --output=/users/xweichu/object_size/data/R0.5MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=read --bs=1024k --directory=/mnt/cephfs/FIO/1MB/ --output-format=json --output=/users/xweichu/object_size/data/R1MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=read --bs=2048k --directory=/mnt/cephfs/FIO/2MB/ --output-format=json --output=/users/xweichu/object_size/data/R2MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=read --bs=4096k --directory=/mnt/cephfs/FIO/4MB/ --output-format=json --output=/users/xweichu/object_size/data/R4MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=read --bs=8192k --directory=/mnt/cephfs/FIO/8MB/ --output-format=json --output=/users/xweichu/object_size/data/R8MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=read --bs=16384k --directory=/mnt/cephfs/FIO/16MB/ --output-format=json --output=/users/xweichu/object_size/data/R16MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=read --bs=32768k --directory=/mnt/cephfs/FIO/32MB/ --output-format=json --output=/users/xweichu/object_size/data/R32MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=read --bs=65536k --directory=/mnt/cephfs/FIO/64MB/ --output-format=json --output=/users/xweichu/object_size/data/R64MB.json --iodepth=1
 
 
-    fio /users/xweichu/FIO/global.fio --rw=write --bs=256k --directory=/mnt/cephfs/FIO/0.25MB/ --output-format=json --output=/users/xweichu/object_size/data/W0.25MB.json --iodepth=1
-    fio /users/xweichu/FIO/global.fio --rw=write --bs=512k --directory=/mnt/cephfs/FIO/0.5MB/ --output-format=json --output=/users/xweichu/object_size/data/W0.5MB.json --iodepth=1
-    fio /users/xweichu/FIO/global.fio --rw=write --bs=1024k --directory=/mnt/cephfs/FIO/1MB/ --output-format=json --output=/users/xweichu/object_size/data/W1MB.json --iodepth=1
-    fio /users/xweichu/FIO/global.fio --rw=write --bs=2048k --directory=/mnt/cephfs/FIO/2MB/ --output-format=json --output=/users/xweichu/object_size/data/W2MB.json --iodepth=1
-    fio /users/xweichu/FIO/global.fio --rw=write --bs=4096k --directory=/mnt/cephfs/FIO/4MB/ --output-format=json --output=/users/xweichu/object_size/data/W4MB.json --iodepth=1
-    fio /users/xweichu/FIO/global.fio --rw=write --bs=8192k --directory=/mnt/cephfs/FIO/8MB/ --output-format=json --output=/users/xweichu/object_size/data/W8MB.json --iodepth=1
-    fio /users/xweichu/FIO/global.fio --rw=write --bs=16384k --directory=/mnt/cephfs/FIO/16MB/ --output-format=json --output=/users/xweichu/object_size/data/W16MB.json --iodepth=1
-    fio /users/xweichu/FIO/global.fio --rw=write --bs=32768k --directory=/mnt/cephfs/FIO/32MB/ --output-format=json --output=/users/xweichu/object_size/data/W32MB.json --iodepth=1
-    fio /users/xweichu/FIO/global.fio --rw=write --bs=65536k --directory=/mnt/cephfs/FIO/64MB/ --output-format=json --output=/users/xweichu/object_size/data/W64MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=write --bs=256k --directory=/mnt/cephfs/FIO/0.25MB/ --output-format=json --output=/users/xweichu/object_size/data/W0.25MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=write --bs=512k --directory=/mnt/cephfs/FIO/0.5MB/ --output-format=json --output=/users/xweichu/object_size/data/W0.5MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=write --bs=1024k --directory=/mnt/cephfs/FIO/1MB/ --output-format=json --output=/users/xweichu/object_size/data/W1MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=write --bs=2048k --directory=/mnt/cephfs/FIO/2MB/ --output-format=json --output=/users/xweichu/object_size/data/W2MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=write --bs=4096k --directory=/mnt/cephfs/FIO/4MB/ --output-format=json --output=/users/xweichu/object_size/data/W4MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=write --bs=8192k --directory=/mnt/cephfs/FIO/8MB/ --output-format=json --output=/users/xweichu/object_size/data/W8MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=write --bs=16384k --directory=/mnt/cephfs/FIO/16MB/ --output-format=json --output=/users/xweichu/object_size/data/W16MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=write --bs=32768k --directory=/mnt/cephfs/FIO/32MB/ --output-format=json --output=/users/xweichu/object_size/data/W32MB.json --iodepth=1
+    # fio /users/xweichu/FIO/global.fio --rw=write --bs=65536k --directory=/mnt/cephfs/FIO/64MB/ --output-format=json --output=/users/xweichu/object_size/data/W64MB.json --iodepth=1
 
-    for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+    for i in 1 2 3 4 5 6 7 8 9 10 
+    # 11 12 13 14 15 16 17 18 19 20 21 22 23
     do
-    fio /users/xweichu/FIO/global.fio --rw=write --bs=4096k --directory=/mnt/cephfs/FIO/4MB/ --output-format=json --output=/users/xweichu/object_size/data/W4MB_$i.json --numjobs=$i
+    fio /users/xweichu/FIO/global.fio --rw=write --bs=4096k --directory=/mnt/cephfs/FIO/4MB/ --output-format=json --output=/users/xweichu/object_size/data/W4MB_$i.json --rate_iops=$i
     done
 
     cd /users/xweichu/object_size
